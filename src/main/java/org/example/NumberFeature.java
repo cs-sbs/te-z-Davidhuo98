2afd05d7df9e0d6df866ca376ed411e1d9427e76
HUOHUANLIN <88132919+Davidhuo98@users.noreply.github.com>
2024-10-26 20:15:31 +0800

    Add files via upload

---
+++ src/main/java/org/example/NumberFeature.java
@@ -0,0 +1,31 @@
+import org.example.NumberFeature;
+import java.util.Scanner;  
+  
+public class NumberFeature {  
+    public static void main(String[] args) {  
+        Scanner scanner = new Scanner(System.in);  
+        int number = scanner.nextInt();  
+        scanner.close();  
+  
+        int result = 0;  
+        int position = 1; // 从个位开始，数位为1  
+  
+     
+        String numberStr = Integer.toString(number);  
+  
+    
+        for (int i = numberStr.length() - 1; i >= 0; i--) {  
+            int digit = numberStr.charAt(i) - '0';   
+            // 检查数位和数字的奇偶性是否一致  
+            if ((digit % 2 == 0 && position % 2 == 0) || (digit % 2 != 0 && position % 2 != 0)) {  
+                
+                result += Math.pow(2, numberStr.length() - 1 - i);  
+                
+            }  
+            position++; // 数位加1  
+        }  
+  
+       
+        System.out.println(result);  
+    }  
+}
