84fe8c2098b9456b522ca62a5d40b5e7a5709998
HUOHUANLIN <88132919+Davidhuo98@users.noreply.github.com>
2024-10-26 16:56:19 +0800

    Update OddEvenFeatureValue.java

--- src/main/java/org/example/OddEvenFeatureValue.java
+++ src/main/java/org/example/OddEvenFeatureValue.java
@@ -1,30 +1,27 @@
-import java.util.Scanner;
-
-public class OddEvenFeatureValue {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int number = scanner.nextInt();
-        scanner.close();
-
-        int result = 0;
-        int position = 1; // 从个位开始，数位为1  
-
-
-        String numberStr = Integer.toString(number);
-
-
-        for (int i = numberStr.length() - 1; i >= 0; i--) {
-            int digit = numberStr.charAt(i) - '0'; // 将字符转换为整数  
-
-            if ((digit % 2 == 0 && position % 2 == 0) || (digit % 2 != 0 && position % 2 != 0)) {
-
-                result += Math.pow(2, numberStr.length() - 1 - i);
-
-            }
-            position++;
-        }
-
-        // 输出结果  
-        System.out.println(result);
-    }
+public class OddEvenFeatureValue {  
+    public static void main(String[] args) {  
+        int number = 342315; 
+        int result = 0;  
+        int binaryPositionValue = 1; 
+       
+        String numberStr = Integer.toString(number);  
+  
+       
+        for (int i = numberStr.length() - 1; i >= 0; i--) {  
+            int digit = numberStr.charAt(i) - '0';
+            int position = numberStr.length() - i;
+  
+          
+            if ((digit % 2 == 0 && position % 2 == 0) || (digit % 2 != 0 && position % 2 != 0)) {  
+                
+                result += binaryPositionValue;  
+            }  
+  
+            
+            binaryPositionValue *= 2;  
+        }  
+  
+          
+        System.out.println(result);  
+    }  
 }
